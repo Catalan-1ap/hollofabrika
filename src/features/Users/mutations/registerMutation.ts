1230819cbf75bdb5d6b927e8c226294b347638c9
Yura Slobodenyuk <catalan.1ap.main@gmail.com>
2023-07-02 10:58:23 +0300

    refactor register

--- src/features/Users/mutations/registerMutation.ts
+++ src/features/Users/mutations/registerMutation.ts
@@ -1,12 +1,18 @@
 import { aql } from "arangojs";
 import { DbRegisterTemporalToken, DbUser } from "../../../infrastructure/dbTypes.js";
-import { querySingle } from "../../../infrastructure/arangoUtils.js";
+import { querySingle, transaction } from "../../../infrastructure/arangoUtils.js";
 import { makeApplicationError } from "../../../infrastructure/formatErrorHandler.js";
-import { GqlErrorCode, GqlMutationResolvers, GqlUser } from "../../../infrastructure/gqlTypes.js";
+import {
+    GqlErrorCode,
+    GqlMutationRegisterArgs,
+    GqlMutationResolvers,
+    GqlUser
+} from "../../../infrastructure/gqlTypes.js";
 import { HollofabrikaContext } from "../../../infrastructure/hollofabrikaContext.js";
 import { mailSender } from "../../../infrastructure/mailSender.js";
 import { generateNumberToken, hashPassword } from "../users.services.js";
 import { getTemporalTokensCollection, getUsersCollection } from "../users.setup.js";
+import * as crypto from "crypto";
 
 
 export const registerMutation: GqlMutationResolvers<HollofabrikaContext>["register"] =
@@ -14,33 +20,14 @@ export const registerMutation: GqlMutationResolvers<HollofabrikaContext>["regist
         const usersCollection = getUsersCollection(context.db);
         const temporalTokensCollection = getTemporalTokensCollection(context.db);
 
-        const trx = await context.db.beginTransaction({
-            read: [usersCollection],
-            write: [temporalTokensCollection]
-        });
-
-        const existed = await trx.step(() => querySingle<DbUser>(context.db, aql`
+        const existedUser = await querySingle<DbUser>(context.db, aql`
 			for doc in ${usersCollection}
 			filter doc.username == ${args.username} or doc.email == ${args.email}
 			return doc
-		`));
+		`);
 
-        if (existed) {
-            const fieldsToCheck = [
-                { name: "username", message: "Register_UsernameInUseError" },
-                { name: "email", message: "Register_EmailInUseError" }
-            ] satisfies {
-                name: keyof GqlUser,
-                message: string
-            }[];
-
-            for (let field of fieldsToCheck) {
-                const origin = existed[field.name];
-
-                if (origin === args[field.name])
-                    throw makeApplicationError(field.message, GqlErrorCode.BadRequest);
-            }
-        }
+        if (existedUser)
+            throwIfFieldsAreDuplicated(existedUser, args);
 
         const hash = await hashPassword(args.password);
 
@@ -54,26 +41,49 @@ export const registerMutation: GqlMutationResolvers<HollofabrikaContext>["regist
                 passwordHash: hash
             },
         };
-        await trx.step(() => context.db.query(aql`
-			upsert ${{ payload: { username: args.username, email: args.email } }}
-			insert ${registerTemporalToken}
-			update ${registerTemporalToken} IN ${temporalTokensCollection}
-		`));
 
-        await mailSender.sendMail({
-            subject: "Email Confirmation",
-            to: args.email,
-            text: `
+        return await transaction(context.db, {
+            write: [temporalTokensCollection]
+        }, async trx => {
+            await trx.step(() => context.db.query(aql`
+		    	upsert ${{ payload: { username: args.username, email: args.email } }}
+		    	insert ${registerTemporalToken}
+		    	update ${registerTemporalToken} IN ${temporalTokensCollection}
+		    `));
+
+            await mailSender.sendMail({
+                subject: "Email Confirmation",
+                to: args.email,
+                text: `
 				Hello, ${args.username}!
 				
 				To verify your e-mail address, please use this key
 				
-				${registerTemporalToken.emailToken}
-			`
-        });
+				${registerTemporalToken.emailToken}`
+            });
 
-        await trx.commit();
-        return {
-            confirmToken: registerTemporalToken.confirmToken
-        };
+            return {
+                data: {
+                    confirmToken: registerTemporalToken.confirmToken
+                }
+            };
+        });
     };
+
+
+function throwIfFieldsAreDuplicated(user: DbUser, args: GqlMutationRegisterArgs) {
+    const fieldsToCheck = [
+        { name: "username", message: "Register_UsernameInUseError" },
+        { name: "email", message: "Register_EmailInUseError" }
+    ] satisfies {
+        name: keyof GqlUser,
+        message: string
+    }[];
+
+    for (let field of fieldsToCheck) {
+        const origin = user[field.name];
+
+        if (origin === args[field.name])
+            throw makeApplicationError(field.message, GqlErrorCode.BadRequest);
+    }
+}
